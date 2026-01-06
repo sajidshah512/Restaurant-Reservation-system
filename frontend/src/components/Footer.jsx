20f48dc2bbd251793f2991ea94c9c6826131aec1
sajidshah512 <tansirshah130@gmail.com>
2026-01-06 19:29:06 +0500

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">The Dome</div>
+          <div className="right">
+            <p>Damane koh, Islamabad</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By SAJID SHAH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By SAJID SHAH.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
