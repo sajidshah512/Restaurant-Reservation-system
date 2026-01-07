c0b7ea836b192f14f2a923307f2016d55d935594
sajidshah512 <tansirshah130@gmail.com>
2026-01-07 20:07:30 +0500

    changes

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
+const backend_URL = "http://localhost:5000/api/v1/reservation/send";
+
 const Reservation = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -18,7 +20,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/reservation/send",
+        backend_URL,
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
